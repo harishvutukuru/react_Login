cb8580c104ecd7f23a3ab865572d9aec7e5b6c98
harishvutukuru <harishvutukuru01@gmail.com>
2023-02-08 15:04:23 -0600

    summary
    commit2

---
+++ react_login/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <body>
+
+      </body>
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;
